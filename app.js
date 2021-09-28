cc74b2fdaeb8b9dcf27213f4f94e8abb37c5a7b2
krowker <krowker97@gmail.com>
2021-09-28 23:40:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const startBtn = document.querySelector ('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+let time = 20
+
+startBtn.addEventListener ('click', (event)=>{
+event.preventDefault()
+screens[0].classList.add('up')
+})
+
+timeList.addEventListener ('click', (event)=> {
+   if(event.target.classList.contains('time-btn')){
+      time = parseInt(event.target.getAttribute('data-time'))
+      screens[1].classList.add('up')
+      startGame()
+   }
+})
+
+//DEBUG
+startGame()
+
+function startGame(){   
+   timeEl.innerHTML=`00:${time}`
+}
